1737624c4d210540bc6656e69bf7b0c7d3a90445
MAY <maythinees59@gmail.com>
2019-09-16 13:37:19 +0700

    Design UI Home Screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -11,12 +11,55 @@ class _HomeState extends State<Home> {
 
 // Method
 
+  Widget singUpButton() {
+    return Expanded(
+      child: OutlineButton(
+        borderSide: BorderSide(color: Colors.red[300]),
+        child: Text(
+          'Sing Up',
+          style: TextStyle(color: Colors.red[300]),
+        ),
+        onPressed: () {},
+      ),
+    );
+  }
+
+  Widget singInButton() {
+    return Expanded(
+      child: RaisedButton(
+        color: Colors.red[300],
+        child: Text(
+          'Sing IN',
+          style: TextStyle(color: Colors.white),
+        ),
+        onPressed: () {},
+      ),
+    );
+  }
+
+  Widget showButton() {
+    return Container(
+      width: 250.0,
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          singInButton(),
+          SizedBox(
+            width: 4.0,
+          ),
+          singUpButton(),
+        ],
+      ),
+    );
+  }
+
   Widget emailText() {
     return Container(
       width: 250.0,
-      child: TextFormField(keyboardType: TextInputType.emailAddress,
+      child: TextFormField(
+        keyboardType: TextInputType.emailAddress,
         decoration: InputDecoration(
-          icon: Icon(Icons.email),
+          icon: Icon(Icons.email, color: Colors.red[400],),
           labelText: 'Email :',
         ),
       ),
@@ -26,9 +69,10 @@ class _HomeState extends State<Home> {
   Widget passwordText() {
     return Container(
       width: 250.0,
-      child: TextFormField(obscureText: true,
+      child: TextFormField(
+        obscureText: true,
         decoration: InputDecoration(
-          icon: Icon(Icons.lock),
+          icon: Icon(Icons.lock, color: Colors.red[400],),
           labelText: 'Password :',
         ),
       ),
@@ -58,15 +102,26 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              showIcon(),
-              showAppName(),
-              emailText(),
-              passwordText(),
-            ],
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: RadialGradient(
+              colors: [Colors.white, Colors.brown[400]],radius: 1.0,
+            ),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                showIcon(),
+                showAppName(),
+                emailText(),
+                passwordText(),
+                SizedBox(
+                  height: 16.0,
+                ),
+                showButton(),
+              ],
+            ),
           ),
         ),
       ),
