1351fd1e1f3335fe65de8c1eacf5175355250cae
MAY <maythinees59@gmail.com>
2019-09-17 09:35:48 +0700

    Register OK

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:locater/screens/my_services.dart';
 import 'package:locater/screens/my_style.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -105,13 +106,53 @@ class _RegisterState extends State<Register> {
         .createUserWithEmailAndPassword(
             email: emailString, password: passwordString)
         .then((response) {
-          print('Register Success');
-        })
-        .catchError((response) {
-          String title = response.code;
-          String massage = response.message;
-          print('title = $title, mssage = $massage');
-        });
+      print('Register Success');
+      setupDisplayName();
+    }).catchError((response) {
+      String title = response.code;
+      String massage = response.message;
+      print('title = $title, mssage = $massage');
+      myAlert(title, massage);
+    });
+  }
+
+  void myAlert(String title, String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: ListTile(
+            leading: Icon(
+              Icons.add_alert,
+              color: Colors.red,size: 36.0,
+            ),
+            title: Text(title),
+          ),
+          content: Text(message),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> setupDisplayName() async {
+    FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+    FirebaseUser firebaseUser = await firebaseAuth.currentUser();
+    UserUpdateInfo userUpdateInfo = UserUpdateInfo();
+    userUpdateInfo.displayName = nameString;
+    firebaseUser.updateProfile(userUpdateInfo);
+
+    MaterialPageRoute materialPageRoute =
+        MaterialPageRoute(builder: (BuildContext context) => MyService());
+    Navigator.of(context)
+        .pushAndRemoveUntil(materialPageRoute, (Route<dynamic> route) => false);
   }
 
   @override
