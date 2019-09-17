a55614a5dbb9f6085616baab4e6dfee85647b524
MAY <maythinees59@gmail.com>
2019-09-17 10:59:12 +0700

    Sign out Succes

--- lib/screens/my_services.dart
+++ lib/screens/my_services.dart
@@ -1,4 +1,8 @@
+import 'dart:ffi';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:locater/screens/home.dart';
 import 'package:locater/screens/my_style.dart';
 
 class MyService extends StatefulWidget {
@@ -7,12 +11,136 @@ class MyService extends StatefulWidget {
 }
 
 class _MyServiceState extends State<MyService> {
+// Explicit
+
+  String loginString = '';
+  FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
+// Method
+  @override
+  void initState() {
+    super.initState();
+    findDisplayName();
+  }
+
+  Future<Void> findDisplayName() async {
+    FirebaseUser firebaseUser = await firebaseAuth.currentUser();
+    setState(() {
+      loginString = firebaseUser.displayName;
+    });
+  }
+
+  Widget processSignOut() {
+    return ListTile(
+      leading: Icon(
+        Icons.exit_to_app,
+        color: Colors.orange,
+      ),
+      title: Text('Sign Out'),
+      subtitle: Text('Sign out and move to authen page'),
+      onTap: () {
+        mySignOut();
+      },
+    );
+  }
+
+  Future<void> mySignOut() async {
+    await firebaseAuth.signOut().then((response) {
+      MaterialPageRoute materialPageRoute = MaterialPageRoute(builder: (BuildContext context) => Home());
+      Navigator.of(context).pushAndRemoveUntil(materialPageRoute, (Route<dynamic> route) => false);
+    });
+  }
+
+  Widget menuInformation() {
+    return ListTile(
+      leading: Icon(
+        Icons.info,
+        color: Colors.blue,
+      ),
+      title: Text('Information'),
+      subtitle: Text('Information of user login'),
+    );
+  }
+
+  Widget menuShowListFood() {
+    return ListTile(
+      leading: Icon(
+        Icons.fastfood,
+        color: Colors.red,
+      ),
+      title: Text('Show List'),
+      subtitle: Text('Show all food in Locater'),
+    );
+  }
+
+  Widget showLogin() {
+    return Text(
+      'Login by $loginString',
+      style: TextStyle(
+        fontStyle: FontStyle.italic,
+      ),
+    );
+  }
+
+  Widget showAppName() {
+    return Text(
+      'Locater',
+      style: TextStyle(
+          fontSize: MyStyle().h1,
+          color: MyStyle().myTextColor,
+          fontWeight: FontWeight.bold),
+    );
+  }
+
+  Widget showImage() {
+    return Container(
+      width: 80.0,
+      height: 80.0,
+      child: Image.asset('images/icon.png'),
+    );
+  }
+
+  Widget myHeadDrawer() {
+    return DrawerHeader(
+      decoration: BoxDecoration(
+        gradient: RadialGradient(
+          colors: [Colors.white, MyStyle().myMainColor],
+          radius: 1.0,
+        ),
+      ),
+      child: Column(
+        children: <Widget>[
+          showImage(),
+          showAppName(),
+          showLogin(),
+        ],
+      ),
+    );
+  }
+
+  Widget myDrawerMenu() {
+    // แทบเมนูข้าง
+    return Drawer(
+      child: ListView(
+        children: <Widget>[
+          myHeadDrawer(),
+          menuShowListFood(), Divider(), //ใส่เส้นแบ่ง
+          menuInformation(),
+          processSignOut(),
+        ],
+      ),
+    ); // listview เลื่อนได้
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: MyStyle().myMainColor,
+      appBar: AppBar(
+        backgroundColor: MyStyle().myMainColor,
         title: Text('My Service'),
-      ),body: Text('body'),
+      ),
+      body: Text('body'),
+      drawer: myDrawerMenu(),
     );
   }
 }
